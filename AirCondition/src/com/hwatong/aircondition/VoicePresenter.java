4b3443887330af68b1ab32882cd32d3116bbae57
zhangjinbo <zhangjinbo@hwatong.com.cn>
2017-11-27 13:53:47 +0800

    Aircondition: 增加语音关闭接口

---
+++ AirCondition/src/com/hwatong/aircondition/VoicePresenter.java
@@ -0,0 +1,36 @@
+package com.hwatong.aircondition;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+public class VoicePresenter {
+	
+	private IVoiceView iView;
+	private VoiceBroadcast voiceBroadcast;
+	
+	private static final String CLOSE_ACTION = "";
+	
+	public VoicePresenter(IVoiceView iView, Context context){
+		this.iView = iView;
+		voiceBroadcast = new VoiceBroadcast();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(CLOSE_ACTION);
+		context.registerReceiver(voiceBroadcast, intentFilter);
+	}
+	
+	private class VoiceBroadcast extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if(CLOSE_ACTION.equals(intent.getAction())) {
+				iView.close();
+			}
+		}
+	}
+	
+	public void unregisterBroadcast(Context context) {
+		context.unregisterReceiver(voiceBroadcast);
+	}
+}
